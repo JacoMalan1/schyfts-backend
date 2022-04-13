6cfe36fb71eb744ce595ed70cb78256244cbf605
Jaco Malan <jacomalan.12@gmail.com>
2022-04-13 16:22:38 +0200

    Added 'extra' parameter to APIResponse constructor

--- APIResponse.ts
+++ APIResponse.ts
@@ -2,8 +2,14 @@ export class APIResponse {
     private status: string;
     private message: string;
 
-    constructor(status: boolean, message: string) {
+    constructor(status: boolean, message: string, extra?: any) {
         this.status = (status) ? "ok" : "error";
         this.message = message;
+
+        if (extra) {
+            for (let key of Object.keys(extra)) {
+                this[key] = extra[key];
+            }
+        }
     }
 }
