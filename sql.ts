f3bb6f435b77c5cbf6420d8087df18a6ce92e878
Jaco Malan <jacomalan.12@gmail.com>
2022-04-13 18:55:26 +0200

    Added functionality for transactions

--- sql.ts
+++ sql.ts
@@ -1,29 +1,66 @@
 import * as mysql from 'mysql'
 import * as fs from 'fs'
 
-export function sqlQuery(query: string, f?: Array<any>): Promise<any[]> {
+export function sqlStartTransaction(con: mysql.Connection): Promise<void> {
+    return new Promise((resolve, reject) => {
+        con.beginTransaction((err) => {
+            if (err)
+                reject(err)
+            else
+                resolve();
+        })
+    });
+}
+
+export function sqlCommit(con: mysql.Connection): Promise<void> {
+    return new Promise((resolve, reject) => {
+        con.commit((err) => {
+            if (err)
+                reject(err)
+            else
+                resolve();
+        })
+    });
+}
+
+export function sqlRollback(con: mysql.Connection): Promise<void> {
+    return new Promise((resolve, reject) => {
+        con.rollback(err => {
+            if (err)
+                reject(err)
+            else
+                resolve();
+        })
+    });
+}
+
+export function sqlQuery(query: string, params?: Array<any>, con?: mysql.Connection): Promise<any[]> {
 
     let SQL_HOST = process.env.SQL_HOST;
     let SQL_USER = process.env.SQL_USER;
     let SQL_PASS = process.env.SQL_PASS;
     let SQL_DB = process.env.SQL_DB;
 
-    let sqlConnection = mysql.createConnection({
-        host: `${SQL_HOST}`,
-        user: `${SQL_USER}`,
-        password: `${SQL_PASS}`,
-        database: `${SQL_DB}`,
-        ssl: {
-            ca: fs.readFileSync('server-ca.pem'),
-            cert: fs.readFileSync('client-cert.pem'),
-            key: fs.readFileSync('client-key.pem')
-        }
-    });
+    let sqlConnection = con;
 
-    if (f) {
+    if (!con) {
+        sqlConnection = mysql.createConnection({
+            host: `${SQL_HOST}`,
+            user: `${SQL_USER}`,
+            password: `${SQL_PASS}`,
+            database: `${SQL_DB}`,
+            ssl: {
+                ca: fs.readFileSync('server-ca.pem'),
+                cert: fs.readFileSync('client-cert.pem'),
+                key: fs.readFileSync('client-key.pem')
+            }
+        });
+    }
+
+    if (params) {
 
         return new Promise((resolve, reject) => {
-            sqlConnection.query(query, f, (err, results) => {
+            sqlConnection.query(query, params, (err, results) => {
                 if (err) {
                     reject(err);
                 } else {
